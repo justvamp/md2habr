0a114971bd1733a67334cbeccba83b9072f3aa77
justvamp <justvamp@gmail.com>
2014-07-13 13:26:00 +0400

    Refactoring

--- MarkdownToHabrahabrConverter.php
+++ MarkdownToHabrahabrConverter.php
@@ -20,43 +20,43 @@ class MarkdownToHabrahabrConverter
     }
 
     /* TODO: refactor */
-    private function convertSources($txt)
+    protected function convertSources($txt)
     {
         $txt = preg_replace('/[\n\r]*```([\w]*)(.+?)```[\n\r]*/s', "\n\n<source lang=\"$1\">$2</source>\n", $txt);
         return $txt;
     }
 
-    private function convertImages($txt)
+    protected function convertImages($txt)
     {
         $txt = preg_replace('/\!\[([^\n\r]+?)\][ \t]*\((.+?)\)/s', '<img src="$2" alt="$1"></img>', $txt);
         return $txt;
     }
 
-    private function convertLinks($txt)
+    protected function convertLinks($txt)
     {
         $txt = preg_replace('/\[([^\n\r]+?)\][ \t]*\((http.+?)\)/s', '<a href="$2">$1</a>', $txt);
         return $txt;
     }
 
-    private function convertHeaders($txt)
+    protected function convertHeaders($txt)
     {
-        $txt = preg_replace('/^\s*#{4}[ \t]+(.+?)\s*$/ms', "<h6>$1</h6>", $txt);
-        $txt = preg_replace('/^\s*#{3}[ \t]+(.+?)\s*$/ms', "<h5>$1</h5>", $txt);
-        $txt = preg_replace('/^\s*#{2}[ \t]+(.+?)\s*$/ms', "<h4>$1</h4>", $txt);
+        $txt = $this->parseStringLiteral($txt, '####', 'h6');
+        $txt = $this->parseStringLiteral($txt, '###', 'h5');
+        $txt = $this->parseStringLiteral($txt, '##', 'h4');
 
         return $txt;
     }
 
-    private function convertInlines($txt)
+    protected function convertInlines($txt)
     {
-        $txt = preg_replace('/(\s+|^)\*{2}([^\*\n\r]+?[^\* \t]+)\*{2}/m', '$1<b>$2</b>', $txt);
-        $txt = preg_replace('/(\s+|^)_([^_\n\r]+?[^_ \t]+)_/m', '$1<i>$2</i>', $txt);
-        $txt = preg_replace('/(\s+|^)`([^`\n\r]+?[^` \t]+)`/m', '$1<code>$2</code>', $txt);
+        $txt = $this->parseInlineLiteral($txt, '**', 'b');
+        $txt = $this->parseInlineLiteral($txt, '_', 'i');
+        $txt = $this->parseInlineLiteral($txt, '`', 'code');
 
         return $txt;
     }
 
-    private function convertLists($txt)
+    protected function convertLists($txt)
     {
         $txt = preg_replace('/((^( *\* +([^\n\r]+)[^\n]+)$[\n]+)+)/m', "<ul>\n$1</ul>\n", $txt);
         $txt = preg_replace('/^( *\* +([^\n\r]+)[^\n]+)$/m', "<li>$2</li>", $txt);
@@ -64,11 +64,45 @@ class MarkdownToHabrahabrConverter
         return $txt;
     }
 
-    private function convertBlockquotes($txt)
+    protected function convertBlockquotes($txt)
     {
         $txt = preg_replace('/((^( *\> +([^\n\r]+)[^\n]+)$\s+)+)/m', "<blockquote>\n$1</blockquote>\n", $txt);
         $txt = preg_replace('/^ *\> +(([^\n\r]+)[^\n]+$)/m', "$1", $txt);
 
         return $txt;
+    }
+
+    protected function parseInlineLiteral($string, $delimiter, $tag)
+    {
+        $u = $this->addSlashes($this->extractUniqueSymbols($delimiter));
+        $d = $this->addSlashes($delimiter);
+        return preg_replace(
+            '/(\s+|^)' . $d . '([^' . $u . '\n\r]+?[^' . $u . ' \t]+)' . $d . '/m',
+            '$1<' . $tag . '>$2</' . $tag . '>',
+            $string
+        );
+    }
+
+    protected function parseStringLiteral($string, $prefix, $tag)
+    {
+        $p = $this->addSlashes($prefix);
+        return preg_replace(
+            '/^\s*' . $p . '[ \t]+(.+?)\s*$/ms',
+            '<' . $tag . '>$1</' . $tag . '>',
+            $string
+        );
+    }
+
+    private function extractUniqueSymbols($string)
+    {
+        $allSymbolsArray = str_split($string);
+        $uniqueSymbolsArray = array_unique($allSymbolsArray);
+
+        return implode('', $uniqueSymbolsArray);
+    }
+
+    private function addSlashes($string)
+    {
+        return preg_replace('/(.)/s', '\\\$1', $string); // abc => /a/b/c
     }
 }
